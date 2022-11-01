cd68a1c0f1ccd9d9802b330dc2829849169e2b45
WarriorFromLongAgo <hubeixuegao@gmail.com>
2022-11-01 19:12:46 +0800

    framework util

--- framework-util/src/test/java/check/AbstractCheckServiceTest.java
+++ framework-util/src/test/java/check/AbstractCheckServiceTest.java
@@ -12,16 +12,16 @@ public class AbstractCheckServiceTest {
     @Test
     public void checkIsNumber() {
         String str = "11";
-        System.out.println(str + " 是不是数字: " + AbstractCheckService.checkIsNumber(str));
+        System.out.println(str + " 是不是数字: " + AbstractCheckService.checkIsNumber("111", str));
         String str1 = "11.1";
-        System.out.println(str1 + " 是不是数字: " + AbstractCheckService.checkIsNumber(str1));
+        System.out.println(str1 + " 是不是数字: " + AbstractCheckService.checkIsNumber("111", str1));
         String str2 = "-11";
-        System.out.println(str2 + " 是不是数字: " + AbstractCheckService.checkIsNumber(str2));
+        System.out.println(str2 + " 是不是数字: " + AbstractCheckService.checkIsNumber("111", str2));
         String str3 = "-11.2";
-        System.out.println(str3 + " 是不是数字: " + AbstractCheckService.checkIsNumber(str3));
+        System.out.println(str3 + " 是不是数字: " + AbstractCheckService.checkIsNumber("111", str3));
 
         String str4 = "11.1a";
-        System.out.println(str4 + " 是不是数字: " + AbstractCheckService.checkIsNumber(str4));
+        System.out.println(str4 + " 是不是数字: " + AbstractCheckService.checkIsNumber("111", str4));
 
     }
 
