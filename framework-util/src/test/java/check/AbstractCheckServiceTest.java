78aa6876723425fb549f7e6eb90c2793f7b63140
WarriorFromLongAgo <hubeixuegao@gmail.com>
2022-11-01 19:18:58 +0800

    framework util

--- framework-util/src/test/java/check/AbstractCheckServiceTest.java
+++ framework-util/src/test/java/check/AbstractCheckServiceTest.java
@@ -2,27 +2,33 @@ package check;
 
 import com.xuegao.util.check.AbstractCheckService;
 import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @author xuegao
  * @date 2022年10月20日 14:46
  */
-public class AbstractCheckServiceTest {
+@SpringBootTest
+public class AbstractCheckServiceTest implements AbstractCheckService {
+
 
     @Test
     public void checkIsNumber() {
         String str = "11";
-        System.out.println(str + " 是不是数字: " + AbstractCheckService.checkIsNumber("111", str));
+        System.out.println(str + " 是不是数字: " + checkIsNumber("111", str));
         String str1 = "11.1";
-        System.out.println(str1 + " 是不是数字: " + AbstractCheckService.checkIsNumber("111", str1));
+        System.out.println(str1 + " 是不是数字: " + checkIsNumber("111", str1));
         String str2 = "-11";
-        System.out.println(str2 + " 是不是数字: " + AbstractCheckService.checkIsNumber("111", str2));
+        System.out.println(str2 + " 是不是数字: " + checkIsNumber("111", str2));
         String str3 = "-11.2";
-        System.out.println(str3 + " 是不是数字: " + AbstractCheckService.checkIsNumber("111", str3));
+        System.out.println(str3 + " 是不是数字: " + checkIsNumber("111", str3));
 
-        String str4 = "11.1a";
-        System.out.println(str4 + " 是不是数字: " + AbstractCheckService.checkIsNumber("111", str4));
-
+        // String str4 = "11.1a";
+        // try {
+        //     System.out.println(str4 + " 是不是数字: " + checkIsNumber("111", str4));
+        // }catch (Exception e){
+        //     log
+        // }
     }
 
 
