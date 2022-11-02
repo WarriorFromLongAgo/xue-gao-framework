fe5e9352ac0166da83c58a986655dee6b45ecc56
WarriorFromLongAgo <hubeixuegao@gmail.com>
2022-11-02 20:24:39 +0800

    framework util

--- framework-core/src/main/java/com/xuegao/core/model/ContextUtil.java
+++ framework-core/src/main/java/com/xuegao/core/model/ContextUtil.java
@@ -1,42 +1,40 @@
-// package com.xuegao.core.model;
-//
-// import com.alibaba.ttl.TransmittableThreadLocal;
-//
-// public class ContextUtil {
-//     private static ThreadLocal<Context> CURRENT_LOCAL_CONTEXT = new TransmittableThreadLocal<>();
-//
-//
-//     public static Context get() {
-//         return CURRENT_LOCAL_CONTEXT.get();
-//     }
-//
-//     public static void set(Context context) {
-//         CURRENT_LOCAL_CONTEXT.set(context);
-//     }
-//
-//     public static void remove() {
-//         CURRENT_LOCAL_CONTEXT.remove();
-//     }
-//
-//     public static void addGlobalParam(String key, Object value) {
-//         Context context = get();
-//         if (context == null) {
-//             set(new Context());
-//             context = get();
-//         }
-//
-//         context.addGlobalParam(key, value);
-//     }
-//
-//     public Object getGlobalParam(String key) {
-//         Object result = null;
-//
-//         Context context = get();
-//         if (context != null) {
-//             result = context.getGLOBAL_PARAM_MAP(key);
-//         }
-//
-//         return result;
-//     }
-//
-// }
+package com.xuegao.core.model;
+
+import com.alibaba.ttl.TransmittableThreadLocal;
+
+public class ContextUtil {
+    private static final ThreadLocal<Context> CURRENT_LOCAL_CONTEXT = new TransmittableThreadLocal<>();
+
+    public static Context get() {
+        return CURRENT_LOCAL_CONTEXT.get();
+    }
+
+    public static void set(Context context) {
+        CURRENT_LOCAL_CONTEXT.set(context);
+    }
+
+    public static void remove() {
+        CURRENT_LOCAL_CONTEXT.remove();
+    }
+
+    public static void addGlobalParam(String key, Object value) {
+        Context context = get();
+        if (context == null) {
+            set(new Context());
+            context = get();
+        }
+
+        context.addGlobalParam(key, value);
+    }
+
+    public Object getGlobalParam(String key) {
+        Object result = null;
+
+        Context context = get();
+        if (context != null) {
+            result = context.getGlobalParam(key);
+        }
+        return result;
+    }
+
+}
