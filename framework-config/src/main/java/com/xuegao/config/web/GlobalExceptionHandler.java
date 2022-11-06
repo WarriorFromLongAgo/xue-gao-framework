9538ba678ee5b6d41a037d7f358c8e146b9b58e2
WarriorFromLongAgo <hubeixuegao@gmail.com>
2022-11-06 13:54:33 +0800

    framework mapper

---
+++ framework-config/src/main/java/com/xuegao/config/web/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.xuegao.config.web;
+
+import com.xuegao.core.model.BaseResult;
+import com.xuegao.core.model.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Throwable.class)
+    public Object handle(Throwable ex) {
+        log.info("[xue-gao-framework][GlobalExceptionHandler][handle][ex={}]", ex.getMessage(), ex);
+        return Result.error(BaseResult.SYSTEM_ERROR);
+    }
+
+    @ExceptionHandler(ServiceException.class)
+    public Object handle(ServiceException ex) {
+        return Result.error(ex.getErrorCode(), ex.getMessage());
+    }
+
+}
---
+++ framework-config/src/main/java/com/xuegao/config/web/ServiceException.java
@@ -0,0 +1,38 @@
+package com.xuegao.config.web;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+//最高父类业务异常
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+public class ServiceException extends RuntimeException {
+
+    private static final long serialVersionUID = 430933593095358673L;
+
+    private String errorMessage;
+
+    private String errorCode;
+
+    /**
+     * 构造新实例。
+     */
+    public ServiceException() {
+        super();
+    }
+
+    /**
+     * 用给定的异常信息构造新实例。
+     *
+     * @param errorMessage 异常信息。
+     */
+    public ServiceException(String errorMessage) {
+        super((String) null);
+        this.errorMessage = errorMessage;
+    }
+
+    //省略部分代码
+
+}
