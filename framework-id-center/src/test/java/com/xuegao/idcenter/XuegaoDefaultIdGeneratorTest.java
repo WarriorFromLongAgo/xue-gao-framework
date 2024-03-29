a905090b91063a681dd8dbe0f4c70b05a5975fed
feijiumeng_zn <feijiumeng@163.com>
2024-03-29 09:21:39 +0800

    id

--- framework-id-center/src/test/java/com/xuegao/idcenter/XuegaoDefaultIdGeneratorTest.java
+++ framework-id-center/src/test/java/com/xuegao/idcenter/XuegaoDefaultIdGeneratorTest.java
@@ -3,14 +3,24 @@ package com.xuegao.idcenter;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class XuegaoDefaultIdGeneratorTest {
 
     @Test
     public void generateTraceId() {
         XuegaoIdGenerator xuegaoDefaultIdGenerator = new XuegaoDefaultIdGenerator();
-        System.out.println(xuegaoDefaultIdGenerator.nextId());
-        System.out.println(xuegaoDefaultIdGenerator.nextId());
-        System.out.println(xuegaoDefaultIdGenerator.nextId());
+
+        List<String> stringList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            String nextId = xuegaoDefaultIdGenerator.nextId();
+            stringList.add(nextId);
+        }
+
+        for (String s : stringList) {
+            System.out.println(s);
+        }
     }
 
 
